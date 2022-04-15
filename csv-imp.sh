7f4e758d9b8a58de08410777b4f63b1d0021e6e8
Sedrez <masedrez@gmail.com>
2022-04-15 17:13:08 -0300

    Fix deploy

---
+++ csv-imp.sh
@@ -0,0 +1,30 @@
+docker info > /dev/null 2>&1
+
+# Ensure that Docker is running...
+if [ $? -ne 0 ]; then
+    echo "Docker is not running."
+
+    exit 1
+fi
+docker run --rm -it \
+	-v $(pwd):/opt \
+	-w /opt laravelsail/php81-composer:latest \
+	/bin/bash -c "composer self-update && composer diagnose && composer install && cp .env.example .env && ./artisan sail:install"
+
+CYAN='\033[0;36m'
+LIGHT_CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m'
+
+echo ""
+
+if sudo -n true 2>/dev/null; then
+    sudo chown -R $USER: .
+    echo -e "${WHITE}Get started with:${NC} ./vendor/bin/sail up"
+else
+    echo -e "${WHITE}Please provide your password so we can make some final adjustments to your application's permissions.${NC}"
+    echo ""
+    sudo chown -R $USER: .
+    echo ""
+    echo -e "${WHITE}Thank you! We hope you build something incredible. Dive in with:${NC} ./vendor/bin/sail up"
+fi
